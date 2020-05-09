39c3cc48fd7013004f80d6fd79dc285bbc2c8a15
Adrian-ACI <aacanacuani@unal.edu.co>
2020-05-09 02:06:43 -0500

    Updates

---
+++ precache-manifest.10ab0d458ecd7130a797b79e5d42b5b3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77dfe217a0bf1cf317701c9792517b24",
+    "url": "/reactexample/index.html"
+  },
+  {
+    "revision": "7db386c1d41525983e84",
+    "url": "/reactexample/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7bbf62c4e073e2534c59",
+    "url": "/reactexample/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "7db386c1d41525983e84",
+    "url": "/reactexample/static/js/2.18495844.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactexample/static/js/2.18495844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbf62c4e073e2534c59",
+    "url": "/reactexample/static/js/main.a9b35e49.chunk.js"
+  },
+  {
+    "revision": "4dd1b74666c0086fd8a9",
+    "url": "/reactexample/static/js/runtime-main.01a750f5.js"
+  },
+  {
+    "revision": "94c2313bb2c178095e202b5eb4f9ebe3",
+    "url": "/reactexample/static/media/banner1.94c2313b.png"
+  },
+  {
+    "revision": "7e9dc173edf2349c9c2631482f3b1938",
+    "url": "/reactexample/static/media/banner2.7e9dc173.png"
+  },
+  {
+    "revision": "3a3f82fc476e9b4ff3f7db0425fcee64",
+    "url": "/reactexample/static/media/banner4.3a3f82fc.png"
+  },
+  {
+    "revision": "baa4a6a9e5cc58e792198f40a0922ae0",
+    "url": "/reactexample/static/media/j1.baa4a6a9.png"
+  },
+  {
+    "revision": "4229248687ab35004cb371eaab8c147f",
+    "url": "/reactexample/static/media/j2.42292486.png"
+  },
+  {
+    "revision": "36cb14d9908386a7a712dbd35448096a",
+    "url": "/reactexample/static/media/j3.36cb14d9.png"
+  },
+  {
+    "revision": "4bdbfb04fd3029a35160ba88c824347b",
+    "url": "/reactexample/static/media/j4.4bdbfb04.png"
+  },
+  {
+    "revision": "78f7d99e780a326ada9a1bb3bedea054",
+    "url": "/reactexample/static/media/j5.78f7d99e.png"
+  }
+]);
--- src/components/NoteList.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class NoteList extends Component {
-  render() {
-    return <div>lista de notas</div>;
-  }
-}
